0569fef4bd7b132684c763bb9b21f9a69dc656ff
Your Name <zhuming3834@sina.com>
2016-07-09 10:15:51 +0800

    Initial Commit

---
+++ CoreAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimation
+//
+//  Created by hgdq on 16/7/9.
+//  Copyright © 2016年 hgdq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreAnimation
+//
+//  Created by hgdq on 16/7/9.
+//  Copyright © 2016年 hgdq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
